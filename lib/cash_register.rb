5c77e3239e6863911d6ef135382a5868d8de1647
Eden Mesfun <emesfun@gmail.com>
2018-02-07 16:45:40 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,11 +2,6 @@ class CashRegister
 
   attr_accessor :new, :total, :discount
 
-  def new
-    @new = new
-    @total = 0
-  end
-
   def initialize(discount)
     @discount = discount
     @new = new
