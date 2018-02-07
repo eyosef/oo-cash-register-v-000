d5b190af9847a8248bf1353cf732bc9591b5fd65
Eden Mesfun <emesfun@gmail.com>
2018-02-07 19:53:28 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,9 @@ class CashRegister
 
   def add_item(item, price, quantity=1)
     self.total += price * quantity
+    quantity.times do
     @@items << item
+  end
     binding.pry
 
     # prev_total = price * quantity
