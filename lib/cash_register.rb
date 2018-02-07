3ff434d6f0c7d4d72dbc1d77498f6fa7ceb5ace1
Eden Mesfun <emesfun@gmail.com>
2018-02-07 17:37:03 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
   def add_item(item, price, quantity=nil)
     quantity ||= nil
     final_price = price * quantity.to_f
-    @total = @total + final_price
+    @total = price * quantity.to_f
   end
 
 end
