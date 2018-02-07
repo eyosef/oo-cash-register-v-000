e6538711855053724ea247ebf8ea0c34c60b999d
Eden Mesfun <emesfun@gmail.com>
2018-02-07 19:44:46 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,7 +19,7 @@ class CashRegister
   def add_item(item, price, quantity=1)
     self.total += price * quantity
     if quantity.length > item
-      then @@items << item until quatity.count == item
+      then @@items << item until quatity.length == item
     end
     # prev_total = price * quantity
     # final_total = prev_total + (price * quantity)
