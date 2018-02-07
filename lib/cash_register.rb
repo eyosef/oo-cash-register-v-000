99f45d3dbb39ed644ef177cf45287ea3d69beb6c
Eden Mesfun <emesfun@gmail.com>
2018-02-07 19:09:43 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,7 +23,7 @@ class CashRegister
   end
 
   def apply_discount
-    discounted_total = total - (total * discount / 100)
+    discounted_total = total - (total * discount / 100) #does equal 800
     discounted_total
   end
 
